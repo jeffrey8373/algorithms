2f330c362060589459cf2cc7243867a4470a30a9
sunhao <alexsun8373@gmail.com>
2020-04-30 09:35:36 +1000

    Done

--- dynamic_programming/max_sum_contiguous_subarray.py
+++ dynamic_programming/max_sum_contiguous_subarray.py
@@ -1,13 +1,17 @@
 import numpy as np
 
 MaxHere = [-1,-1,-1,-1,-1,-1,-1]
+
 def dp_maxhere(A,i):
     if i == 0:
-        return max(A[i],0)
-    if i > 1:
-        return max(MaxHere[i-1] + A[i],0)
+        MaxHere[i] = max(A[i],0)
+        return MaxHere[i]
+    if i > 0:
+        MaxHere[i]= max(dp_maxhere(A,i-1) + A[i],0)
+        return MaxHere[i]
 
 
 if __name__ == '__main__':
-    A = [-2, 5, -1, -5, 3, -1, 2]
+    A = [2, -6, -1, 3, -1, 2, -2]
     dp_maxhere(A,6)
+    print(MaxHere)
